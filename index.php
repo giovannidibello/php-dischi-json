8e93a98c023acb87d710c8aa98722b4477ea34c8
Giovanni <dibellogio@gmail.com>
2025-05-02 15:44:05 +0200

    fix split PHP tags per line

--- index.php
+++ index.php
@@ -29,34 +29,37 @@ require_once "./server.php";
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- importo bootstrap -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-    <title>Lista Dischi</title>
+    <!-- importo css -->
+    <link rel="stylesheet" href="style.css">
+    <title>Albums</title>
 </head>
 
 <body>
 
-    <h1 class="text-center">Albums</h1>
-    <hr>
+
+    <header>
+        <img src="./img/logo-small.svg" alt="Logo">
+    </header>
 
     <div class="container">
-        <div class="row">
-            <?php
-            foreach ($albums as $album) {
-                echo '<div class="col-12 col-sm-6 col-md-4 mb-4">';
-                echo '<div class="card" style="width: 100%;">';
-                echo '<img src="' . $album['cover_url'] . '" class="card-img-top" alt="' . $album['title'] . '">';
-                echo '<div class="card-body text-center">';
-                echo '<h6 class="card-title">' . $album['title'] . '</h6>';
-                echo '<h6 class="card-subtitle mb-2 text-muted">' . $album['artist'] . '</h6>';
-                echo '<p class="card-text">' . $album['release_year'] . '</p>';
-                echo '</div>';
-                echo '</div>';
-                echo '</div>';
-            }
-            ?>
+        <div class="row justify-content-center">
+            <?php foreach ($albums as $album) { ?>
+                <div class="col-12 col-sm-6 col-md-4 mb-4">
+                    <div class="card mx-auto" style="width: 80%;">
+                        <img class="card-img-top" src="<?php echo $album['cover_url']; ?>" alt="<?php echo $album['title']; ?>">
+                        <div class="card-body text-center">
+                            <h6 class="card-title"><?php echo $album['title']; ?></h6>
+                            <h6 class="card-subtitle mb-2"><?php echo $album['artist']; ?></h6>
+                            <p class="card-text"><?php echo $album['release_year']; ?></p>
+                        </div>
+                    </div>
+                </div>
+            <?php } ?>
+
         </div>
 
 
-        <form action="./server.php" method="POST" class="p-4 mb-4 border rounded bg-light">
+        <form action="./server.php" method="POST" class="p-4 mb-4 border rounded">
             <div class="row mb-3">
                 <div class="col-md-6 mb-2">
                     <input type="text" class="form-control" name="title" placeholder="Titolo" required>
