ab6268d7c70af6515cdb7ed532d4cc8ade7297be
Giovanni <dibellogio@gmail.com>
2025-05-02 13:02:31 +0200

    add logic for insert new album

--- server.php
+++ server.php
@@ -8,4 +8,25 @@ $json_text = file_get_contents("./albums.json");
 // coverto la stringa da json a struttura dati php
 $albums = json_decode($json_text, true);
 
-// var_dump($albums);
+// modifico la struttura dati
+// inserisco il nuovo album nel file
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    $newAlbum = [
+        "title" => $_POST["title"],
+        "artist" => $_POST["artist"],
+        "cover_url" => $_POST["cover_url"],
+        "release_year" => $_POST["release_year"],
+        "genre" => $_POST["genre"]
+    ];
+
+    $albums[] = $newAlbum;
+
+    // riconverto la struttura dati php in stringa json
+    $json_text_updated = json_encode($albums);
+
+    // sovrascrivo il contenuto del file json
+    file_put_contents("./albums.json", $json_text_updated);
+
+    // reindirizzo l'utente alla index
+    header("Location: ./index.php");
+}
